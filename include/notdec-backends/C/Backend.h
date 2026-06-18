9d16e422e06836b3e9791a55e1c74f47f1f90b70
am009 <warrenwjk@qq.com>
2026-06-18 15:46:43 +0000

    backend: expose c backend target

---
+++ include/notdec-backends/C/Backend.h
@@ -0,0 +1,26 @@
+#ifndef NOTDEC_BACKENDS_C_BACKEND_H
+#define NOTDEC_BACKENDS_C_BACKEND_H
+
+#include <utility>
+
+#include "notdec-llvm2c/Interface.h"
+
+namespace notdec::backend::c {
+
+using Options = ::notdec::llvm2c::Options;
+using StructuralAlgorithms = ::notdec::llvm2c::StructuralAlgorithms;
+using HTypeResult = ::notdec::llvm2c::HTypeResult;
+
+inline void decompileModule(llvm::Module &M, llvm::ModuleAnalysisManager &MAM,
+                            llvm::raw_fd_ostream &OS, Options Opts,
+                            std::unique_ptr<HTypeResult> HT = nullptr) {
+  ::notdec::llvm2c::decompileModule(M, MAM, OS, Opts, std::move(HT));
+}
+
+inline void demoteSSA(llvm::Module &M, llvm::ModuleAnalysisManager &MAM) {
+  ::notdec::llvm2c::demoteSSA(M, MAM);
+}
+
+} // namespace notdec::backend::c
+
+#endif
