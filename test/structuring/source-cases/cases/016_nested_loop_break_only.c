d8403514004397dc7f9cb9bb1861c1fda3a5326b
am009 <warrenwjk@qq.com>
2026-07-01 01:01:25 +0000

    sailr: add nested loop break-only source case

---
+++ test/structuring/source-cases/cases/016_nested_loop_break_only.c
@@ -0,0 +1,20 @@
+extern int next(void);
+extern void sink(int);
+
+int nested_loop_break_only(int outer, int inner) {
+  int total = 0;
+  while (outer > 0) {
+    int j = inner;
+    while (j > 0) {
+      int x = next();
+      if (x < 0) {
+        break;
+      }
+      total += x;
+      --j;
+    }
+    sink(total);
+    --outer;
+  }
+  return total;
+}
